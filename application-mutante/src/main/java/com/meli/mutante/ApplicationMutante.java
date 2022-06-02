27ed065defb369657b95a1a6908dde08c5e3b616
Jose Curiel <josecuriel88@gmail.com>
2022-06-02 13:57:20 -0500

    init

---
+++ application-mutante/src/main/java/com/meli/mutante/ApplicationMutante.java
@@ -0,0 +1,13 @@
+package com.meli.mutante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMutante {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMutante.class, args);
+	}
+
+}
